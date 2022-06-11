c1bc58a181931a9e4262abf898570644c2837274
Pavel Guzenfeld <pavelguzenfeld@gmail.com>
2022-06-11 16:41:26 +0300

    housekeeping

--- inc/vm.hpp
+++ inc/vm.hpp
@@ -1,24 +1,24 @@
 #ifndef VM_HPP
 #define VM_HPP
 
-#include <string>
-#include <array>
+#include "settings.hpp"
 
 namespace chip8
 {
 
-std::string m_title = "Chip8 Emulator";
-size_t const m_memorySize = 4096;
-size_t const m_width = 64;
-size_t const m_hight = 32;
-size_t const m_windowScale = 10;
-
-struct Memory
+class vm
 {
-    void set(size_t a_index, unsigned char a_value);
-    std::array<char, m_memorySize> m_memory;
-};
+    Stack<u_short, STACK_CAPACITY> m_stack;
+    Registers m_vRegisters;
+    Memory m_memory;
+    u_short m_index;
 
+    u_char m_delayTimer;
+    u_char m_soundTimer;
+
+    u_short m_PC;
+    
+};
 
 }   //namespace chip8
 
