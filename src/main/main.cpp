4b7fde42aff8ba9ffb16739241f441e948d05842
Pavel Guzenfeld <pavelguzenfeld@gmail.com>
2022-06-15 22:31:02 +0300

    Moved main to seporate folder

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,14 +1,15 @@
-//#include <SDL2/SDL.h>
+#include <iostream>
 #include "io.hpp"
+#include "vm.hpp"
 
 int main(int argc, char** argv)
-{
+{   
     using namespace chip8;
     auto window = Canvas{460, 320};
     auto renderer = Renderer{window};
     renderer.drawRect();
     renderer.present();
-    
+
     loop();
     
     return 0;
