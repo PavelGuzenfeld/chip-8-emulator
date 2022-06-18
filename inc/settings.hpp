69dee179a26027c1abfba25818e5014e64005584
Pavel Guzenfeld <pavelguzenfeld@gmail.com>
2022-06-18 22:37:17 +0300

    switched over to stl array

--- inc/settings.hpp
+++ inc/settings.hpp
@@ -3,7 +3,7 @@
 
 #include <string>
 #include <unordered_map>
-#include "lib/array.hpp"
+#include <array>
 #include "lib/stack.hpp"
 
 namespace chip8
@@ -13,25 +13,25 @@ using U8Bit = unsigned char;
 using U16Bit = unsigned short;
 
 U16Bit const MEMORY_CAPACITY = 4096;
-using Memory = Array<U8Bit, MEMORY_CAPACITY>;
+using Memory = std::array<U8Bit, MEMORY_CAPACITY>;
 
 U16Bit const VX_CAPACITY = 16;
-using Vx = Array<U8Bit, VX_CAPACITY>;
+using Vx = std::array<U8Bit, VX_CAPACITY>;
 
 U16Bit const KEYS_CAPACITY = 16;
-using Keys = Array<U8Bit, KEYS_CAPACITY>;
+using Keys = std::array<U8Bit, KEYS_CAPACITY>;
 using KeysMap = std::unordered_map<U8Bit, U8Bit>;
 
 U16Bit const STACK_CAPACITY = 16;
 using CallStack = Stack<U16Bit, STACK_CAPACITY>;
 
 U16Bit const CHAR_SET_CAPACITY = 16 * 5;
-using CharSet = Array<U8Bit, CHAR_SET_CAPACITY>;
+using CharSet = std::array<U8Bit, CHAR_SET_CAPACITY>;
 
 U16Bit const CANVAS_WIDTH = 64;
 U16Bit const CANVAS_HEIGHT = 32;
 U16Bit const CANVAS_SCALE = 10;
-using Pixels = Array<U8Bit, CANVAS_WIDTH * CANVAS_WIDTH>;
+using Pixels = std::array<U8Bit, CANVAS_WIDTH * CANVAS_WIDTH>;
 
 std::string const TITLE = "Chip8 Emulator";
 
