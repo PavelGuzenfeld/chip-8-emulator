0a26d72e43fb74fe9467ac208588c9ed22aeb6b7
Pavel Guzenfeld <pavelguzenfeld@gmail.com>
2022-09-23 03:02:23 +0300

    added default value to m_pc

--- inc/registers.hpp
+++ inc/registers.hpp
@@ -1,22 +1,21 @@
 #ifndef REGISTERS_HPP
 #define REGISTERS_HPP
 
-#include"settings.hpp"
+#include "settings.hpp"
 
 namespace chip8
 {
 
-struct Registers
-{
+    struct Registers
+    {
 
-    Vx m_vx;
-    U16Bit m_index;
-    U16Bit m_PC;
-    U8Bit m_delayTimer;
-    U8Bit m_soundTimer;
-    
-};
+        Vx m_vx;
+        U16Bit m_index;
+        U16Bit m_PC = CODE_LOAD_ADDRESS;
+        U8Bit m_delayTimer;
+        U8Bit m_soundTimer;
+    };
 
-}   //namespace chip8
+} // namespace chip8
 
-#endif  //REGISTERS_HPP
+#endif // REGISTERS_HPP
